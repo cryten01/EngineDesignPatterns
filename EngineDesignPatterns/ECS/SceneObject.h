aa80f08254966ee44bb89e1ad6083faccd705ecc
cryten01 <cryten101@gmail.com>
2020-02-11 18:21:20 +0100

    Moved main outside of ECS folder

---
+++ EngineDesignPatterns/ECS/SceneObject.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "System.h"
+
+struct SceneObject : Entity 
+{
+
+};
--- EngineDesignPatterns/main.cpp
+++ EngineDesignPatterns/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-#include "System.h"
-#include "Transform.h"
-#include "Mesh.h"
+#include "ECS/System.h"
+#include "ECS/Transform.h"
+#include "ECS/Mesh.h"
 
 
-int main()
+void RunECSTest() 
 {
 	Entity entity;
 	TransformData transData1;
@@ -16,26 +16,31 @@ int main()
 	meshData.points.push_back(2);
 	meshData.points.push_back(3);
 	meshData.points.push_back(5);
+
 	transData1.x = 2;
 	transData1.y = 2;
 	transData1.z = 2;
+	
 	transData2.x = 3;
 	transData2.y = 4;
 	transData2.z = 5;
-
 
 	System::AddData<TransformData>(transData1, entity.dataList);
 	System::AddData<TransformData>(transData2, entity.dataList);
 	System::AddData<MeshData>(meshData, entity.dataList);
 
-
 	TransformData transform = System::GetData<TransformData>(entity.dataList);
 	std::vector<TransformData> transform2 = System::GetAllData<TransformData>(entity.dataList);
 	MeshData mesh = System::GetData<MeshData>(entity.dataList);
-
 
 	std::cout << System::HasData<MeshData>(entity.dataList) << std::endl;
 	std::cout << transform.x << std::endl;
 	std::cout << transform2.at(1).y << std::endl;
 	std::cin.get();
+}
+
+
+int main()
+{
+	RunECSTest();
 }
